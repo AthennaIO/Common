0b8395141b918aab3829168367dee45a74731586
João Lenon <lenonSec7@gmail.com>
2022-10-20 18:52:33 -0300

    chore(copyright): change the email

--- src/Helpers/Exec.js
+++ src/Helpers/Exec.js
@@ -1,7 +1,7 @@
 /**
  * @athenna/common
  *
- * (c) João Lenon <lenonSec7@gmail.com>
+ * (c) João Lenon <lenon@athenna.io>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
