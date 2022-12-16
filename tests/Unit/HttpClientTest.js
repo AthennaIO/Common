5f59b39f627effb42bdaa60f208c91c8a4095d89
João Lenon <lenonSec7@gmail.com>
2022-12-16 19:35:27 -0300

    test(client): add tests for GET and POST requests

--- tests/Unit/HttpClientTest.js
+++ tests/Unit/HttpClientTest.js
@@ -1,26 +1,63 @@
-import { test } from '@japa/runner'
-import { HttpClient } from '../../src/index.js'
-const client = new HttpClient()
+/**
+ * @athenna/common
+ *
+ * // TODO Add your email here
+ * (c) Robson Trasel <...>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 
-test.group('Verbs HTTP ', () => {
-  test('GET request', async ({ assert }) => {
-    const response = await client.get('http://example.com/')
-    assert.equal(response.statusCode, 200)
+import { test } from '@japa/runner'
+import { HttpClient, HttpClientBuilder } from '#src/Helpers/HttpClient'
+
+const FAKE_API_URL = 'http://localhost:9999'
+
+test.group('HttpClientTest', group => {
+  group.each.setup(async () => {
+    const client = new HttpClientBuilder()
+
+    client.prefixUrl(FAKE_API_URL)
+
+    HttpClient.setBuilder(client)
   })
-  test('POST request', async ({ assert }) => {
-    const response = await client.post('http://example.com', { message: 'Hello' })
-    assert.equal(response.statusCode, 200)
+
+  test('should be able to make a GET request using HttpClient', async ({ assert }) => {
+    const users = await HttpClient.get('users').json()
+
+    assert.lengthOf(users, 3)
+    assert.deepEqual(users[0], { id: 1, name: 'Robson Trasel' })
   })
-  test('PUT request', async ({ assert }) => {
-    const response = await client.put('http://example.com', { message: 'Hello' })
-    assert.equal(response.statusCode, 200)
+
+  test('should be able to make a POST request using HttpClient', async ({ assert }) => {
+    const userCreated = await HttpClient.post('/users', { name: 'Robson Trasel' }).json()
+
+    assert.deepEqual(userCreated, { id: 1, name: 'Robson Trasel' })
   })
-  test('DELETE request', async ({ assert }) => {
-    const response = await client.delete('http://example.com')
-    assert.equal(response.statusCode, 200)
+
+  test('should be able to make a GET request using HttpClient request builder', async ({ assert }) => {
+    const users = await HttpClient.builder()
+      .prefixUrl(FAKE_API_URL)
+      .method('GET')
+      .header('Accept', 'application/json')
+      .url('/users')
+      .request()
+      .json()
+
+    assert.lengthOf(users, 3)
+    assert.deepEqual(users[0], { id: 1, name: 'Robson Trasel' })
   })
-  test('PATCH request', async ({ assert }) => {
-    const response = await client.patch('http://example.com', { message: 'Hello' })
-    assert.equal(response.statusCode, 200)
+
+  test('should be able to make a POST request using HttpClient request builder', async ({ assert }) => {
+    const userCreated = await HttpClient.builder()
+      .prefixUrl(FAKE_API_URL)
+      .method('post')
+      .header('Accept', 'application/json')
+      .safeHeader('Content-Type', 'application/json')
+      .url('/users')
+      .request()
+      .json()
+
+    assert.deepEqual(userCreated, { id: 1, name: 'Robson Trasel' })
   })
 })
