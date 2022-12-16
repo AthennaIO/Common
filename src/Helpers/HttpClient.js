ff325f9822f6631dd579a0e60c2e6462c76f0e9e
João Lenon <lenonSec7@gmail.com>
2022-12-16 19:33:34 -0300

    feat(client): add implementation using builder pattern

--- src/Helpers/HttpClient.js
+++ src/Helpers/HttpClient.js
@@ -1,51 +1,431 @@
 import got from 'got'
+import { Is } from '#src/Helpers/Is'
+
+/**
+ * TODO Finish to implement the all the options from https://github.com/sindresorhus/got/blob/main/source/core/options.ts#L977
+ *
+ * Example: We have not implemented the "followRedirect" method of Got Options in HttpClientBuilder
+ *
+ * The implementation would be like this:
+ *
+ * export class HttpClientBuilder {
+ *   followRedirect(followRedirect) {
+ *     this.#options.followRedirect = followRedirect
+ *
+ *     return this
+ *   }
+ * }
+ *
+ * TODO We need to do this type of implementation with most part of methods of Got Options class
+ */
+export class HttpClientBuilder {
+  /**
+   * Got options used to make the request.
+   *
+   * @type {import('got').Options}
+   */
+  #options
+
+  /**
+   * Creates a new instance of HttpClientBuilder.
+   */
+  constructor() {
+    this.#options = {}
+  }
+
+  /**
+   * Set the request url.
+   *
+   * @param url {string}
+   * @return {HttpClientBuilder}
+   */
+  url(url) {
+    if (url.startsWith('/')) {
+      url = url.replace('/', '')
+    }
+
+    this.#options.url = url
+
+    return this
+  }
+
+  /**
+   * Set the request method.
+   *
+   * @param method {import('got').Method}
+   * @return {HttpClientBuilder}
+   */
+  method(method) {
+    this.#options.method = method
+
+    return this
+  }
+
+  /**
+   * When specified, `prefixUrl` will be prepended to `url`.
+   * The prefix can be any valid URL, either relative or absolute.
+   * A trailing slash `/` is optional - one will be added automatically.
+   *
+   * __Note__: `prefixUrl` will be ignored if the `url` argument is a URL instance.
+   *
+   * __Note__: Leading slashes in `input` are disallowed when using this option to enforce consistency and avoid confusion.
+   * For example, when the prefix URL is `https://example.com/foo` and the input is `/bar`, there's ambiguity whether the resulting URL would become `https://example.com/foo/bar` or `https://example.com/bar`.
+   * The latter is used by browsers.
+   *
+   * __Tip__: Useful when used with `got.extend()` to create niche-specific Got instances.
+   *
+   * __Tip__: You can change `prefixUrl` using hooks as long as the URL still includes the `prefixUrl`.
+   * If the URL doesn't include it anymore, it will throw.
+   *
+   * @example
+   * ```
+   * await HttpClient.builder()
+   *    .prefixUrl('https://cats.com')
+   *    .get('unicorn')
+   *    .json()
+   * //=> 'https://cats.com/unicorn'
+   * ```
+   * @param prefixUrl {string}
+   * @return {HttpClientBuilder}
+   */
+  prefixUrl(prefixUrl) {
+    this.#options.prefixUrl = prefixUrl
+
+    return this
+  }
+
+  /**
+   * Set the request body.
+   *
+   * @param body {any}
+   * @return {HttpClientBuilder}
+   */
+  body(body) {
+    if (Is.Object(body)) {
+      this.#options.json = body
+
+      return this
+    }
+
+    this.#options.body = body
+
+    return this
+  }
+
+  /**
+   * Set the request form.
+   *
+   * @param form {any}
+   * @return {HttpClientBuilder}
+   */
+  form(form) {
+    this.#options.form = form
+
+    return this
+  }
+
+  /**
+   * Set a header at the request.
+   *
+   * @param key {string}
+   * @param value {string}
+   * @return {HttpClientBuilder}
+   */
+  header(key, value) {
+    if (!this.#options.headers) {
+      this.#options.headers = {}
+    }
+
+    this.#options.headers[key] = value
+
+    return this
+  }
+
+  /**
+   * Set a header at the request only if is not already
+   * defined.
+   *
+   * @param key {string}
+   * @param value {string}
+   * @return {HttpClientBuilder}
+   */
+  safeHeader(key, value) {
+    if (!this.#options.headers) {
+      this.#options.headers = {}
+    }
+
+    if (this.#options.headers[key]) {
+      return this
+    }
+
+    this.#options.headers[key] = value
+
+    return this
+  }
+
+  /**
+   * Remove a header from the request.
+   *
+   * @param key {string}
+   * @return {HttpClientBuilder}
+   */
+  removeHeader(key) {
+    if (!this.#options.headers) {
+      this.#options.headers = {}
+    }
+
+    if (!this.#options.headers[key]) {
+      return this
+    }
+
+    delete this.#options.headers[key]
+
+    return this
+  }
+
+  /**
+   * Decompress the response automatically.
+   *
+   * This will set the `accept-encoding` header to `gzip, deflate, br` unless you set it yourself.
+   *
+   * If this is disabled, a compressed response is returned as a `Buffer`.
+   * This may be useful if you want to handle decompression yourself or stream the raw compressed data.
+   *
+   * @param decompress {boolean}
+   */
+  decompress(decompress) {
+    this.#options.decompress = decompress
+
+    return this
+  }
+
+  /**
+   * Milliseconds to wait for the server to end the response before aborting the request with `got.TimeoutError` error (a.k.a. `request` property).
+   *
+   * By default, there's no timeout.
+   *
+   * This also accepts an `object` with the following fields to constrain the duration of each phase of the request lifecycle:
+   *
+   * - `lookup` starts when a socket is assigned and ends when the hostname has been resolved.
+   *     Does not apply when using a Unix domain socket.
+   * - `connect` starts when `lookup` completes (or when the socket is assigned if lookup does not apply to the request) and ends when the socket is connected.
+   * - `secureConnect` starts when `connect` completes and ends when the handshaking process completes (HTTPS only).
+   * - `socket` starts when the socket is connected. See [request.setTimeout](https://nodejs.org/api/http.html#http_request_settimeout_timeout_callback).
+   * - `response` starts when the request has been written to the socket and ends when the response headers are received.
+   * - `send` starts when the socket is connected and ends with the request has been written to the socket.
+   * - `request` starts when the request is initiated and ends when the response's end event fires.
+   * @param delays {Partial<import('got').Delays>}
+   */
+  timeout(delays) {
+    this.#options.timeout = delays
+
+    return this
+  }
+
+  /**
+   * Set the request retry strategy.
+   *
+   * @param strategy {Partial<import('got').RetryOptions>}
+   */
+  retryStrategy(strategy) {
+    this.#options.retry = strategy
+
+    return this
+  }
+
+  /**
+   * Execute the request using all the options defined.
+   *
+   * @param [options] {import('got').Options}
+   * @return {import('got').CancelableRequest<any>}
+   */
+  request(options = {}) {
+    return got({ ...this.#options, ...options })
+  }
+
+  /**
+   * Make a GET request.
+   *
+   * @param [url] {string}
+   * @param [options] {import('got').Options}
+   * @return {import('got').CancelableRequest<any>}
+   */
+  get(url, options = {}) {
+    this.method('GET').url(url || options.url || this.#options.url)
+
+    return this.request(options)
+  }
+
+  /**
+   * Make a POST request.
+   *
+   * @param [url] {string}
+   * @param [body] {any}
+   * @param [options] {import('got').Options}
+   * @return {import('got').CancelableRequest<any>}
+   */
+  post(url, body, options = {}) {
+    this.method('POST')
+      .url(url || options.url || this.#options.url)
+      .body(body || options.body || this.#options.body || {})
+
+    return this.request(options)
+  }
+
+  /**
+   * Make a PUT request.
+   *
+   * @param url {string}
+   * @param [body] {any}
+   * @param [options] {import('got').Options}
+   * @return {import('got').CancelableRequest<any>}
+   */
+  put(url, body, options = {}) {
+    this.method('PUT')
+      .url(url || options.url || this.#options.url)
+      .body(body || options.body || this.#options.body || {})
+
+    return this.request(options)
+  }
+
+  /**
+   * Make a PATCH request.
+   *
+   * @param url {string}
+   * @param [body] {any}
+   * @param [options] {import('got').Options}
+   * @return {import('got').CancelableRequest<any>}
+   */
+  patch(url, body, options = {}) {
+    this.method('PATCH')
+      .url(url || options.url || this.#options.url)
+      .body(body || options.body || this.#options.body || {})
+
+    return this.request(options)
+  }
+
+  /**
+   * Make a DELETE request.
+   *
+   * @param url {string}
+   * @param [options] {import('got').Options}
+   * @return {import('got').CancelableRequest<any>}
+   */
+  delete(url, options = {}) {
+    this.method('DELETE').url(url || options.url || this.#options.url)
+
+    return this.request(options)
+  }
+
+  /**
+   * Make a HEAD request.
+   *
+   * @param url {string}
+   * @param [options] {import('got').Options}
+   * @return {import('got').CancelableRequest<any>}
+   */
+  head(url, options = {}) {
+    this.method('HEAD').url(url || options.url || this.#options.url)
+
+    return this.request(options)
+  }
+}
 
 export class HttpClient {
-  constructor() {
-    this.globalSettings = {}
+  /**
+   * The global builder used in all HttpClient static requests.
+   *
+   * @type {HttpClientBuilder}
+   */
+  static #builder = new HttpClientBuilder()
+
+  /**
+   * Set the global builder for HttpClient.
+   *
+   * @param builder {HttpClientBuilder}
+   * @return {typeof HttpClient}
+   */
+  static setBuilder(builder) {
+    this.#builder = builder
+
+    return this
   }
 
-  setGlobalSettings(settings) {
-    this.setGlobalSettings = settings
+  /**
+   * Creates an instance of HttpClientBuilder.
+   *
+   * @return {HttpClientBuilder}
+   */
+  static builder() {
+    return new HttpClientBuilder()
   }
 
-  async makeRequest(method, url, options = {}) {
-    const mergedOptions = {
-      ...this.globalSettings,
-      ...options,
-    }
-
-    try {
-      const response = await got(url, { ...mergedOptions, method })
-      return {
-        statusCode: response.statusCode,
-        body: response.body,
-      }
-    } catch (error) {
-      return {
-        statusCode: error.response.statusCode,
-        body: error.response.body,
-      }
-    }
+  /**
+   * Make a GET request.
+   *
+   * @param url {string}
+   * @param [options] {import('got').Options}
+   * @return {import('got').CancelableRequest<any>}
+   */
+  static get(url, options) {
+    return this.#builder.get(url, options)
   }
 
-  async get(url, options = {}) {
-    return this.makeRequest('GET', url, options)
+  /**
+   * Make a POST request.
+   *
+   * @param url {string}
+   * @param [body] {any}
+   * @param [options] {import('got').Options}
+   * @return {import('got').CancelableRequest<any>}
+   */
+  static post(url, body, options) {
+    return this.#builder.post(url, body, options)
   }
 
-  async post(url, body, options = {}) {
-    return this.makeRequest('POST', url, { body, ...options })
+  /**
+   * Make a PUT request.
+   *
+   * @param url {string}
+   * @param [body] {any}
+   * @param [options] {import('got').Options}
+   * @return {import('got').CancelableRequest<any>}
+   */
+  static put(url, body, options) {
+    return this.#builder.put(url, body, options)
   }
 
-  async delete(url, options = {}) {
-    return this.makeRequest('DELETE', url, options)
+  /**
+   * Make a PATCH request.
+   *
+   * @param url {string}
+   * @param [body] {any}
+   * @param [options] {import('got').Options}
+   * @return {import('got').CancelableRequest<any>}
+   */
+  static patch(url, body, options) {
+    return this.#builder.patch(url, body, options)
   }
 
-  async patch(url, body, options = {}) {
-    return this.makeRequest('PATCH', url, { body, ...options })
+  /**
+   * Make a DELETE request.
+   *
+   * @param url {string}
+   * @param [options] {import('got').Options}
+   * @return {import('got').CancelableRequest<any>}
+   */
+  static delete(url, options) {
+    return this.#builder.delete(url, options)
   }
 
-  async put(url, body, options = {}) {
-    return this.makeRequest('PUT', url, { body, ...options })
+  /**
+   * Make a HEAD request.
+   *
+   * @param url {string}
+   * @param [options] {import('got').Options}
+   * @return {import('got').CancelableRequest<any>}
+   */
+  static head(url, options) {
+    return this.#builder.head(url, options)
   }
 }
