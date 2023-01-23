d9df3e080dd3ead8f84ac491b0ffb80c74113b52
João Lenon <lenonsec7@gmail.com>
2023-01-23 12:45:24 -0300

    refactor(json): move Object helper methods to Json

--- src/Helpers/Json.ts
+++ src/Helpers/Json.ts
@@ -9,7 +9,108 @@
 
 import lodash from 'lodash'
 
+import { Is } from './Is.js'
+import { Options } from './Options.js'
+
+export type ObjectBuilderOptions = {
+  /**
+   * The global default value that is going
+   * to be used if the value that is being set
+   * is undefined or null.
+   *
+   * @default null
+   */
+  defaultValue?: any
+
+  /**
+   * Ignore null values that are going to be set
+   * in the object. If some value is null, it will
+   * not be set inside the object.
+   *
+   * @default false
+   */
+  ignoreNull?: boolean
+
+  /**
+   * Ignore undefined values that are going to be set
+   * in the object. If some value is undefined, it will
+   * not be set inside the object.
+   *
+   * @default true
+   */
+  ignoreUndefined?: boolean
+}
+
+export class ObjectBuilder {
+  /**
+   * The real object that is being built.
+   */
+  private object: any
+
+  /**
+   * The object builder options that are
+   * going to be used to shape the object.
+   */
+  private options: ObjectBuilderOptions
+
+  public constructor(options?: ObjectBuilderOptions) {
+    this.options = Options.create(options, {
+      ignoreNull: false,
+      ignoreUndefined: true,
+      defaultValue: null,
+    })
+
+    this.object = {}
+  }
+
+  /**
+   * Set a value in the object or fallback to defaultValue.
+   *
+   * @example
+   *  const object = Object.builder()
+   *    .set('hello.world', 'hello world!')
+   *    .get()
+   *
+   *  console.log(object.hello.world)
+   */
+  public set(key: string, value: any, defaultValue?: any): this {
+    const [mainKey, ...keys] = key.split('.')
+
+    if (Is.Defined(defaultValue) && !Is.Defined(value)) {
+      this.object[mainKey] = lodash.set(
+        {},
+        keys.join('.'),
+        Json.copy(defaultValue),
+      )
+
+      return this
+    }
+
+    if (this.options.ignoreNull && value === null) {
+      return this
+    }
+
+    if (this.options.ignoreUndefined && value === undefined) {
+      return this
+    }
+
+    this.object[key] = lodash.set({}, keys.join('.'), Json.copy(value))
+
+    return this
+  }
+}
+
 export class Json {
+  /**
+   * Create a new instance of ObjectBuilder class.
+   * This class is responsible to build new objects
+   * in a more easy way, removing null and undefined values
+   * and setting default values in options.
+   */
+  public static builder(options?: ObjectBuilderOptions): ObjectBuilder {
+    return new ObjectBuilder(options)
+  }
+
   /**
    * Deep copy any object properties without reference.
    */
