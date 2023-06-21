c0228d673e925a20dc29fbfdabe34172bc710bc5
João Lenon <lenonSec7@gmail.com>
2023-06-21 08:36:44 -0300

    test(number): refactor to use @athenna/test

--- tests/unit/NumberTest.ts
+++ tests/unit/NumberTest.ts
@@ -8,17 +8,19 @@
  */
 
 import { Number } from '#src'
-import { test } from '@japa/runner'
+import { Test, Context } from '@athenna/test'
 
-test.group('Numbers Class', () => {
-  test('should get the higher and lower number from an array of numbers', async ({ assert }) => {
+export default class NumberTest {
+  @Test()
+  public async shouldGetTheHigherAndLowerNumberFromAnArrayOfNumbers({ assert }: Context) {
     const numbersArray = [1, 2, 3, 4, 5]
 
     assert.equal(Number.getLower(numbersArray), 1)
     assert.equal(Number.getHigher(numbersArray), 5)
-  })
+  }
 
-  test('should get km radius between two coordinates', async ({ assert }) => {
+  @Test()
+  public async shouldGetKmRadiusBetweenTwoCoordinates({ assert }: Context) {
     const first = Number.getKmRadius(
       { latitude: -25503207, longitude: -545390592 },
       { latitude: -25503207, longitude: -545390592 },
@@ -30,9 +32,10 @@ test.group('Numbers Class', () => {
 
     assert.equal(first, 0)
     assert.equal(second, 5338.683217695541)
-  })
+  }
 
-  test('should extract all numbers inside of a string in number and array format', async ({ assert }) => {
+  @Test()
+  public async shouldExtractAllNumbersInsideOfAStringInNumberAndArrayFormat({ assert }: Context) {
     const stringWithNumbers =
       "My name is João Lenon and I've 21 years old and I've been living in Foz do Iguaçu for 19 years"
 
@@ -41,9 +44,10 @@ test.group('Numbers Class', () => {
 
     assert.equal(oneBigNumber, 2119)
     assert.deepEqual(numbersArray, [21, 19])
-  })
+  }
 
-  test('should get the average of numbers by args or array of numbers', async ({ assert }) => {
+  @Test()
+  public async shouldGetTheAverageOfNumbersByArgsOrArrayOfNumbers({ assert }: Context) {
     const arrayNumbers = [1, 2, 3, 4, 5]
 
     const argsAverage = Number.argsAverage(arrayNumbers[0], arrayNumbers[1])
@@ -51,10 +55,11 @@ test.group('Numbers Class', () => {
 
     assert.equal(argsAverage, 1.5)
     assert.equal(arrayAverage, 3)
-  })
+  }
 
-  test('should generate a random integer between two integer values', async ({ assert }) => {
+  @Test()
+  public async shouldGenerateARandomIntegerBetweenTwoIntegerValues({ assert }: Context) {
     assert.isNumber(Number.randomIntFromInterval(0, 0))
     assert.isNumber(Number.randomIntFromInterval(1, 10))
-  })
-})
+  }
+}
