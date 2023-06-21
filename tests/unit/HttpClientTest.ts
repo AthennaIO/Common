e16eece9648b7109f969cbd57f85d47d1a55a082
João Lenon <lenonSec7@gmail.com>
2023-06-21 07:45:19 -0300

    test(http-client): refactor to use @athenna/test

--- tests/unit/HttpClientTest.ts
+++ tests/unit/HttpClientTest.ts
@@ -7,25 +7,22 @@
  * file that was distributed with this source code.
  */
 
-import stream from 'node:stream'
-
-import { Response } from 'got'
-import { test } from '@japa/runner'
 import { promisify } from 'node:util'
-import { File } from '#src/helpers/File'
-import { Path } from '#src/helpers/Path'
-import { FakeApi, HttpClient, HttpClientBuilder } from '#src'
+import { pipeline } from 'node:stream'
+import { File, Path, FakeApi, HttpClient, HttpClientBuilder } from '#src'
+import { Test, Context, AfterAll, BeforeAll, BeforeEach } from '@athenna/test'
 
-const pipeline = promisify(stream.pipeline)
+export default class HttpClientTest {
+  private pipeline = promisify(pipeline)
+  private FAKE_API_URL = 'http://localhost:8989'
 
-const FAKE_API_URL = 'http://localhost:8989'
-
-test.group('HttpClientTest', group => {
-  group.setup(async () => {
+  @BeforeAll()
+  public async beforeAll() {
     await FakeApi.start(8989, Path.stubs('resources/fake-api'))
-  })
+  }
 
-  group.each.setup(async () => {
+  @BeforeEach()
+  public async beforeEach() {
     const builder = new HttpClientBuilder()
 
     builder
@@ -33,7 +30,7 @@ test.group('HttpClientTest', group => {
       .setHost(true)
       .timeout(10000)
       .dnsCache(false)
-      .prefixUrl(FAKE_API_URL)
+      .prefixUrl(this.FAKE_API_URL)
       .allowGetBody(false)
       .encoding('utf-8')
       .decompress(true)
@@ -56,21 +53,23 @@ test.group('HttpClientTest', group => {
       })
 
     HttpClient.setBuilder(builder)
-  })
+  }
 
-  group.teardown(async () => {
+  @AfterAll()
+  public async afterAll() {
     await FakeApi.stop()
-  })
+  }
 
-  test('should only be able to set options in builder and reuse in next requests using the setBuilder method', async ({
+  @Test()
+  public async shouldBeOnlyBeAbleToSetOptionsInBuilderAndReuseInNextRequestsUsingTheSetBuildMethod({
     assert,
-  }) => {
+  }: Context) {
     await HttpClient.builder().responseType('json').url('/users').get().json()
 
     const mainBuilder = HttpClient.builder()
     const mainOptions = mainBuilder.getOptions()
 
-    assert.deepEqual(mainOptions.prefixUrl, FAKE_API_URL)
+    assert.deepEqual(mainOptions.prefixUrl, this.FAKE_API_URL)
 
     HttpClient.setBuilder(HttpClient.builder().responseType('json').url('/users'))
 
@@ -78,12 +77,13 @@ test.group('HttpClientTest', group => {
     const testOptions = testBuilder.getOptions()
 
     assert.deepEqual(testOptions.url, 'users')
-    assert.deepEqual(testOptions.prefixUrl, FAKE_API_URL)
+    assert.deepEqual(testOptions.prefixUrl, this.FAKE_API_URL)
     assert.deepEqual(testOptions.responseType, 'json')
     assert.isDefined(HttpClient.setBuilder(mainBuilder).builder().getOptions().prefixUrl)
-  })
+  }
 
-  test('should be able to cache requests', async ({ assert }) => {
+  @Test()
+  public async shouldBeAbleToCacheRequests({ assert }: Context) {
     const storage = new Map()
     const mainBuilder = HttpClient.builder()
 
@@ -102,61 +102,77 @@ test.group('HttpClientTest', group => {
     assert.isDefined(storage.get('cacheable-request:GET:https://sindresorhus.com'))
 
     HttpClient.setBuilder(mainBuilder)
-  })
+  }
 
-  test('should be able to get the response body as object using response type json', async ({ assert }) => {
+  @Test()
+  public async shouldBeAbleToGetTheResponseBodyAsObjectUsingResponseTypeJson({ assert }: Context) {
+    const storage = new Map()
     const mainBuilder = HttpClient.builder()
-    const builder = HttpClient.builder().responseType('json')
+
+    const builder = new HttpClientBuilder()
+      .url('https://sindresorhus.com')
+      .cache(storage)
+      .cacheOptions({ immutableMinTimeToLive: 1000 })
 
     HttpClient.setBuilder(builder)
 
-    const response = await HttpClient.get('users')
+    const notFromCache = await HttpClient.get()
+    const fromCache = await HttpClient.get()
 
-    assert.deepEqual(response.body[0], { id: 1, name: 'Robson Trasel' })
+    assert.isFalse(notFromCache.isFromCache)
+    assert.isTrue(fromCache.isFromCache)
+    assert.isDefined(storage.get('cacheable-request:GET:https://sindresorhus.com'))
 
     HttpClient.setBuilder(mainBuilder)
-  })
+  }
 
-  test('should be able to make a GET request using HttpClient', async ({ assert }) => {
+  @Test()
+  public async shouldBeAbleToMakeAGETRequestUsingHttpClient({ assert }: Context) {
     const users = await HttpClient.get('users').json()
 
     assert.lengthOf(users, 2)
     assert.deepEqual(users[0], { id: 1, name: 'Robson Trasel' })
-  })
+  }
 
-  test('should be able to make a HEAD request using HttpClient', async ({ assert }) => {
+  @Test()
+  public async shouldBeAbleToMakeAHEADRequestUsingHttpClient({ assert }: Context) {
     const response = await HttpClient.head('users')
 
     assert.deepEqual(response.statusCode, 200)
-  })
+  }
 
-  test('should be able to make a POST request using HttpClient', async ({ assert }) => {
+  @Test()
+  public async shouldBeAbleToMakeAPOSTRequestUsingHttpClient({ assert }: Context) {
     const userCreated = await HttpClient.post('/users', { name: 'Robson Trasel' }).json()
 
     assert.deepEqual(userCreated, { id: 1, name: 'Robson Trasel' })
-  })
+  }
 
-  test('should be able to make a PUT request using HttpClient', async ({ assert }) => {
+  @Test()
+  public async shouldBeAbleToMakeAPUTRequestUsingHttpClient({ assert }: Context) {
     const userUpdated = await HttpClient.put('/users/1', { name: 'Robson Trasel Updated' }).json()
 
     assert.deepEqual(userUpdated, { id: 1, name: 'Robson Trasel Updated' })
-  })
+  }
 
-  test('should be able to make a PATCH request using HttpClient', async ({ assert }) => {
+  @Test()
+  public async shouldBeAbleToMakeAPATCHRequestUsingHttpClient({ assert }: Context) {
     const userUpdated = await HttpClient.patch('/users/1', { name: 'Robson Trasel Updated Patch' }).json()
 
     assert.deepEqual(userUpdated, { id: 1, name: 'Robson Trasel Updated Patch' })
-  })
+  }
 
-  test('should be able to make a DELETE request using HttpClient', async ({ assert }) => {
+  @Test()
+  public async shouldBeAbleToMakeADELETERequestUsingHttpClient({ assert }: Context) {
     const response = await HttpClient.delete('/users/1')
 
     assert.deepEqual(response.statusCode, 204)
-  })
+  }
 
-  test('should be able to make a GET request using HttpClient request builder', async ({ assert }) => {
+  @Test()
+  public async shouldBeAbleToMakeAGETRequestUsingHttpClientRequestBuilder({ assert }: Context) {
     const users = await HttpClient.builder(true)
-      .prefixUrl(FAKE_API_URL)
+      .prefixUrl(this.FAKE_API_URL)
       .method('GET')
       .header('Accept', 'application/json')
       .url('/users')
@@ -165,22 +181,24 @@ test.group('HttpClientTest', group => {
 
     assert.lengthOf(users, 2)
     assert.deepEqual(users[0], { id: 1, name: 'Robson Trasel' })
-  })
+  }
 
-  test('should be able to make a HEAD request using HttpClient request builder', async ({ assert }) => {
+  @Test()
+  public async shouldBeAbleToMakeAHEADRequestUsingHttpClientRequestBuilder({ assert }: Context) {
     const response = await HttpClient.builder(true)
-      .prefixUrl(FAKE_API_URL)
+      .prefixUrl(this.FAKE_API_URL)
       .method('HEAD')
       .header('Accept', 'application/json')
       .url('/users')
       .request()
 
     assert.deepEqual(response.statusCode, 200)
-  })
+  }
 
-  test('should be able to make a POST request using HttpClient request builder', async ({ assert }) => {
+  @Test()
+  public async shouldBeAbleToMakeAPOSTRequestUsingHttpClientRequestBuilder({ assert }: Context) {
     const userCreated = await HttpClient.builder(true)
-      .prefixUrl(FAKE_API_URL)
+      .prefixUrl(this.FAKE_API_URL)
       .method('post')
       .header('Accept', 'application/json')
       .url('/users')
@@ -188,11 +206,12 @@ test.group('HttpClientTest', group => {
       .json()
 
     assert.deepEqual(userCreated, { id: 1, name: 'Robson Trasel' })
-  })
+  }
 
-  test('should be able to make a PUT request using HttpClient request builder', async ({ assert }) => {
+  @Test()
+  public async shouldBeAbleToMakeAPUTRequestUsingHttpClientRequestBuilder({ assert }: Context) {
     const userUpdated = await HttpClient.builder(true)
-      .prefixUrl(FAKE_API_URL)
+      .prefixUrl(this.FAKE_API_URL)
       .method('put')
       .header('Accept', 'application/json')
       .url('/users/1')
@@ -200,11 +219,12 @@ test.group('HttpClientTest', group => {
       .json()
 
     assert.deepEqual(userUpdated, { id: 1, name: 'Robson Trasel Updated' })
-  })
+  }
 
-  test('should be able to make a PATCH request using HttpClient request builder', async ({ assert }) => {
+  @Test()
+  public async shouldBeAbleToMakeAPATCHRequestUsingHttpClientRequestBuilder({ assert }: Context) {
     const userUpdated = await HttpClient.builder(true)
-      .prefixUrl(FAKE_API_URL)
+      .prefixUrl(this.FAKE_API_URL)
       .method('patch')
       .header('Accept', 'application/json')
       .url('/users/1')
@@ -212,20 +232,22 @@ test.group('HttpClientTest', group => {
       .json()
 
     assert.deepEqual(userUpdated, { id: 1, name: 'Robson Trasel Updated Patch' })
-  })
+  }
 
-  test('should be able to make a DELETE request using HttpClient request builder', async ({ assert }) => {
+  @Test()
+  public async shouldBeAbleToMakeADELETERequestUsingHttpClientRequestBuilder({ assert }: Context) {
     const response = await HttpClient.builder(true)
-      .prefixUrl(FAKE_API_URL)
+      .prefixUrl(this.FAKE_API_URL)
       .method('delete')
       .header('Accept', 'application/json')
       .url('/users/1')
       .request()
 
     assert.deepEqual(response.statusCode, 204)
-  })
+  }
 
-  test('should be able to setup init hooks for requests', async ({ assert }) => {
+  @Test()
+  public async shouldBeAbleToSetupInitHooksForRequests({ assert }: Context) {
     const builder = HttpClient.builder()
       .setInitHook((plain: any) => {
         assert.isTrue('followRedirects' in plain)
@@ -239,9 +261,10 @@ test.group('HttpClientTest', group => {
       .mergeOptions({ followRedirects: true } as any)
 
     await builder.get('/users').json()
-  })
+  }
 
-  test('should be able to setup before request hooks for requests', async ({ assert }) => {
+  @Test()
+  public async shouldBeAbleToSetupBeforeRequestHooksForRequests({ assert }: Context) {
     const builder = HttpClient.builder().setBeforeRequestHook(options => {
       const body = options.body as string
 
@@ -253,9 +276,10 @@ test.group('HttpClientTest', group => {
     })
 
     await builder.post('users', '{"payload":"old"}')
-  })
+  }
 
-  test('should be able to setup before redirect hooks for requests', async ({ assert }) => {
+  @Test()
+  public async shouldBeAbleToSetupBeforeRedirectHooksFoRequests({ assert }: Context) {
     const builder = HttpClient.builder().setBeforeRedirectHook((options: any, _response) => {
       assert.deepEqual(options.hostname, 'deadSite')
 
@@ -265,31 +289,10 @@ test.group('HttpClientTest', group => {
     })
 
     await builder.get('users')
-  })
+  }
 
-  test('should be able to setup before redirect hooks for requests', async ({ assert }) => {
-    const builder = HttpClient.builder(true)
-      .responseType('json')
-      .setBeforeErrorHook(error => {
-        const response: Response<{ message?: string }> = error.response
-
-        if (response && response.body) {
-          error.name = 'GitHubError'
-          error.message = `${response.body.message} (${response.statusCode})`
-        }
-
-        return error
-      })
-
-    try {
-      await builder.get('https://api.github.com/repos/AthennaIO/NotFound/commits')
-    } catch (err) {
-      assert.deepEqual(err.name, 'GitHubError')
-      assert.deepEqual(err.message, 'Not Found (404)')
-    }
-  })
-
-  test('should be able to setup before retry hooks for requests', async ({ assert }) => {
+  @Test()
+  public async shouldBeAbleToSetupBeforeRetryHooksForRequests({ assert }: Context) {
     const builder = HttpClient.builder().setBeforeRetryHook((error, _retryCount) =>
       assert.deepEqual(error.name, 'ERR_NON_2XX_3XX_RESPONSE'),
     )
@@ -299,9 +302,10 @@ test.group('HttpClientTest', group => {
     } catch (err) {
       assert.deepEqual(err.name, 'RequestError')
     }
-  })
+  }
 
-  test('should be able to setup after response hooks for requests', async ({ assert }) => {
+  @Test()
+  public async shouldBeAbleToSetupAfterResponseHooksForRequests({ assert }: Context) {
     await HttpClient.builder()
       .setAfterResponseHook(response => {
         assert.deepEqual(response.statusCode, 200)
@@ -312,18 +316,19 @@ test.group('HttpClientTest', group => {
       .removeHeader('Content-Type')
       .removeHeader('Content-Type')
       .get('/users')
-  })
+  }
 
-  test('should be able to make a get request and get the response as stream', async ({ assert }) => {
+  @Test()
+  public async shouldBeAbleToMakeAGetRequestAndGetTheResponseAsStream({ assert }: Context) {
     const requestStream = HttpClient.builder().url('users').stream()
     const file = new File(Path.stubs('streamed.json'), Buffer.from(''))
 
-    await pipeline(requestStream, file.createWriteStream())
+    await this.pipeline(requestStream, file.createWriteStream())
     await file.load({ withContent: true })
 
     assert.isTrue(file.fileExists)
     assert.deepEqual(file.content.toString(), '[{"id":1,"name":"Robson Trasel"},{"id":2,"name":"Victor Tesoura"}]')
 
     await File.safeRemove(Path.stubs('streamed.json'))
-  })
-})
+  }
+}
