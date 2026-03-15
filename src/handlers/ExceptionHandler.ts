0606114bf67218fef1e4de9d973c4db06e1962c2
jlenon7 <lenonSec7@gmail.com>
2026-03-15 14:46:11 -0300

    feat(handler): remove error throw

--- src/handlers/ExceptionHandler.ts
+++ src/handlers/ExceptionHandler.ts
@@ -1,3 +1,12 @@
+/**
+ * @athenna/common
+ *
+ * (c) João Lenon <lenon@athenna.io>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 import { Macroable } from '#src/helpers/Macroable'
 
 export type ExceptionHandlerContext = {
@@ -6,6 +15,9 @@ export type ExceptionHandlerContext = {
 
 export class ExceptionHandler extends Macroable {
   public async handle(_: ExceptionHandlerContext): Promise<void> {
-    throw new Error('Not implemented')
+    /**
+     * This method is meant to be overridden by the user 
+     * using the `macro()` method
+     */
   }
 }
